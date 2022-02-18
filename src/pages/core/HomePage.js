42bf69a401b321152986ffeb8f85928741cb9306
Zuber Shekh <zuber.shekh7@gmail.com>
2022-02-18 21:21:53 +0530

    update home page

--- src/pages/core/HomePage.js
+++ src/pages/core/HomePage.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
     <main>
-      <h1>Welcome to Local Shoppers</h1>
+      <section>
+        <div className="grid-span-1 p-20 space-y-4 mx-auto text-center">
+          <h1 className="text-indigo-500 text-5xl md:text-7xl lg:text-9xl font-extrabold">
+            Local Shoppers
+          </h1>
+          <h4 className="text-xl md:text-3xl lg:text-5xl text-gray-600 mt-10">
+            Online Store For Locals, By Locals
+          </h4>
+          <div>
+            <Link
+              to="/"
+              className="inline-block py-3 px-4 text-xl hover:bg-indigo-400 bg-indigo-500 rounded-lg text-white shadow-lg mt-5"
+            >
+              Start selling online
+            </Link>
+          </div>
+        </div>
+      </section>
     </main>
   );
 };
