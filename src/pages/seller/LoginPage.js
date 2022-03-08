68e139a3be5d1eb346753a3b213e707b23703334
Zuber Shekh <zuber.shekh7@gmail.com>
2022-03-08 20:39:10 +0530

    update color scheme and look on login page

--- src/pages/seller/LoginPage.js
+++ src/pages/seller/LoginPage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { sellerLogin } from "../../actions/sellerActions";
+import { HiOutlineShoppingBag } from "react-icons/hi";
 import routes from "../../utils/routes";
 
 const LoginPage = () => {
@@ -30,54 +31,57 @@ const LoginPage = () => {
   };
 
   return (
-    <main className="bg-indigo-400">
-      <section className="container mt-0">
-        <div className="flex justify-center py-5 ">
-          <div className="w-96 bg-indigo-600 rounded-xl shadow-lg p-5">
-            <h2 className="text-center text-white">Login</h2>
-            <form onSubmit={handleSubmit}>
-              <div className="mb-5">
-                <label htmlFor="email">Email</label>
-                <input
-                  className="w-full py-2 px-2 rounded-lg focus:ring-1 focus:ring-white "
-                  type="email"
-                  id="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  placeholder="seller@example.com"
-                  required
-                />
-              </div>
-              <div className="mb-5">
-                <label htmlFor="password">Password</label>
-                <input
-                  className="w-full py-2 px-2 rounded-lg focus:ring-1 focus:ring-white "
-                  type="password"
-                  id="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  placeholder="seller@example.com"
-                  required
-                />
-              </div>
-              <div className="mb-5">
-                <button
-                  className="btn text-base bg-indigo-600 w-full border border-white hover:bg-indigo-700"
-                  type="submit"
-                >
-                  Log In
-                </button>
-              </div>
-              <div className="mb-5">
-                <p className="text-center text-white">
-                  Don't have an account?{" "}
-                  <Link className="underline" to={routes.signup}>
-                    Signup
-                  </Link>
-                </p>
-              </div>
-            </form>
-          </div>
+    <main className="bg-indigo-600">
+      <section className="flex flex-col justify-center  items-center h-screen">
+        <div className="w-96 bg-white rounded-xl shadow-lg p-10">
+          <h2 className="flex gap-x-1 text-3xl text-indigo-600">
+            <HiOutlineShoppingBag className="h-8 w-8" />
+            <span className="font-bold ">Local Shoppers</span>
+          </h2>
+
+          <h1 className="my-4">Login</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-5">
+              <label htmlFor="email">Email</label>
+              <input
+                className="w-full text-lg py-2 px-2 rounded-lg border-2 border-indigo-600  focus:ring-indigo-600"
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="seller@example.com"
+                required
+              />
+            </div>
+            <div className="mb-5">
+              <label htmlFor="password">Password</label>
+              <input
+                className="w-full text-lg py-2 px-2 rounded-lg border-2 border-indigo-600  focus:ring-indigo-600"
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="don't share your password"
+                required
+              />
+            </div>
+            <div className="mb-5">
+              <button
+                className="btn text-lg bg-indigo-600 w-full border hover:bg-indigo-700"
+                type="submit"
+              >
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
+        <div className="mt-5">
+          <p className="text-center text-white text-xl">
+            Don't have an account?{" "}
+            <Link className="underline" to={routes.signup}>
+              Signup
+            </Link>
+          </p>
         </div>
       </section>
     </main>
