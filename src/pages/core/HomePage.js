5beb52152e5486bd07c54a6e4e17a5a26ac04e38
Zuber Shekh <zuber.shekh7@gmail.com>
2022-03-08 20:14:02 +0530

    update hero banner of home page

--- src/pages/core/HomePage.js
+++ src/pages/core/HomePage.js
@@ -18,16 +18,33 @@ const HomePage = () => {
   return (
     <main>
       <section className="container">
-        <div className="flex justify-center">
-          <div>
-            <h1 className="display-1 text-brand text-center">Local Shoppers</h1>
-            <h4 className="text-gray-500 text-xs text-center">
-              Online Store For Locals, By Locals
-            </h4>
-            <div className="text-center mt-5">
-              <Link to={routes.signup} className="btn">
-                Sell Online
-              </Link>
+        <div className="grid grid-cols-12 gap-3">
+          <div className="flex justify-start items-center col-span-12 md:col-span-6">
+            <div>
+              <h1 hidden className="display-1 text-white text-center">
+                Local Shoppers
+              </h1>
+              <h2 className="text-5xl mb-5">Start selling online.</h2>
+              <h4 className="text-xl font-thin mb-5">
+                Create and grow your online business with local shoppers.
+              </h4>
+              <div className="mt-10">
+                <Link
+                  to={routes.signup}
+                  className="btn font-semibold text-2xl bg-indigo-600 text-white py-4 px-5 hover:bg-indigo-500"
+                >
+                  Get Started
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="col-span-12 md:col-span-6">
+            <div className="flex items-center">
+              <img
+                className="rounded-md shadow mt-10"
+                src="https://images.pexels.com/photos/935756/pexels-photo-935756.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                alt="Grow your business with local shoppers"
+              />
             </div>
           </div>
         </div>
