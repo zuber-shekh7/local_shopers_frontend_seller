f8497c4bff9fe89e7242633d763f159507d82e2d
Zuber Shekh <zuber.shekh7@gmail.com>
2022-03-09 01:19:40 +0530

    add mobile responsiveness for notfound page

--- src/pages/core/NotFoundPage.js
+++ src/pages/core/NotFoundPage.js
@@ -6,14 +6,15 @@ const NotFoundPage = () => {
   const navigate = useNavigate();
 
   return (
-    <main className="flex flex-col items-center mt-10 h-screen">
-      <h1 className="text-2xl lg:text-4xl mb-5">
-        Look like you are lost, let me help to go home
+    <main className="container flex flex-col items-center mt-10 h-screen">
+      <img className="w-64 sm:w-80 mb-5" src={NotFound} alt="Page not found" />
+      <h1 className="text-xl sm:text-2xl lg:text-4xl mt-5">
+        Looks like you are lost, <br className="block sm:hidden" />
+        let me help to go home
       </h1>
-      <img className="w-80 mb-5" src={NotFound} alt="Page not found" />
       <div className="mt-5">
         <button
-          className="py-3 px-3 bg-indigo-500 text-white rounded-lg text-bold text-lg hover:text-indigo-400 transition duration-300"
+          className="py-3 px-3 bg-indigo-600 text-white rounded-lg text-bold text-lg hover:bg-indigo-700 transition duration-300"
           onClick={() => navigate(-1)}
         >
           Way to Home
