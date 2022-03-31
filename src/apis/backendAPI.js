9fd73d95c61920aee47761ff0afd32b1dfdb3b62
Zuber Shekh <zuber.shekh7@gmail.com>
2022-03-31 19:53:42 +0530

    update backendAPI configuration

--- src/apis/backendAPI.js
+++ src/apis/backendAPI.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-const BACKEND_SERVER_URL = `${process.env.REACT_APP_BACKEND_SERVER_URL}/api`;
+const BACKEND_SERVER_URL = `${process.env.REACT_APP_BACKEND_SERVER_URL}`;
 
 export default axios.create({
   baseURL: BACKEND_SERVER_URL,
+  withCredentials: true,
 });
