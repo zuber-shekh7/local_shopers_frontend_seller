aa999c8778e9f7a5327c321477e066cd15f638fd
Zuber Shekh <zuber.shekh7@gmail.com>
2022-03-31 21:11:17 +0530

    change design of category page

--- src/pages/categories/CategoryPage.js
+++ src/pages/categories/CategoryPage.js
@@ -9,6 +9,8 @@ import {
 import { deleteCategory, getCategory } from "../../actions/categoriesActions";
 import routes from "../../utils/routes";
 import Modal from "../../components/shared/Modal";
+import Breadcrumb from "../../components/shared/Breadcrumb";
+import moment from "moment";
 
 const CategoryPage = () => {
   const [open, setOpen] = useState(false);
@@ -40,6 +42,23 @@ const CategoryPage = () => {
   return (
     <main>
       <section className="m-10 px-10 max-w-xl mx-auto min-h-screen">
+        {/* breadcrumb */}
+        <Breadcrumb
+          links={[
+            {
+              name: "your account",
+              to: routes.dashboard,
+            },
+            {
+              name: "categories",
+              to: routes.getCategories,
+            },
+            {
+              name: "Category",
+              to: "",
+            },
+          ]}
+        />
         {category && (
           <div className="flex justify-center">
             <Modal
@@ -50,45 +69,42 @@ const CategoryPage = () => {
               description="Once you delete, you won't be able to access it further."
               cta="Confirm Delete"
             />
-            <div className="flex-1 bg-gray-50 border-2 border-gray-50 px-10 py-5 rounded-lg shadow-lg">
-              <div>
-                <div className="flex justify-between">
+            <div className="flex-1 flex flex-col card border-0 hover:bg-none">
+              <div
+                className="h-64 w-full hover:opacity-90 transition duration-500 p-2 mb-3"
+                style={{
+                  backgroundImage: `url(${category.image})`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                }}
+              >
+                <div className="flex justify-end gap-x-2">
                   <Link
-                    className="inline-block p-2 bg-white-100 border-2 border-gray-500 rounded-full text-gray-500 mb-5"
-                    to={routes.getCategories}
+                    className="bg-white rounded-full p-2 shadow-sm text-indigo-600"
+                    to={`${routes.getCategories}/${category._id}/edit`}
                   >
                     <span>
-                      <HiOutlineArrowSmLeft className="h-6 w-6" />
+                      <HiOutlinePencil className="h-6 w-6" />
                     </span>
                   </Link>
-                  <div className="flex space-x-2 items-center">
-                    <Link
-                      className="inline-block p-2 bg-white-100 border-2 border-gray-500 rounded-full text-gray-500 mb-5"
-                      to={`${routes.getCategories}/${category._id}/edit`}
-                    >
-                      <span>
-                        <HiOutlinePencil className="h-6 w-6" />
-                      </span>
-                    </Link>
-                    <button
-                      className="inline-block p-2 bg-white-100 border-2 border-gray-500 rounded-full text-gray-500 mb-5"
-                      onClick={() => setOpen(true)}
-                    >
-                      <span>
-                        <HiOutlineTrash className="h-6 w-6" />
-                      </span>
-                    </button>
-                  </div>
+                  <button
+                    className="bg-white rounded-full p-2 shadow-sm text-red-500"
+                    onClick={() => setOpen(true)}
+                  >
+                    <span>
+                      <HiOutlineTrash className="h-6 w-6" />
+                    </span>
+                  </button>
                 </div>
               </div>
               <div className="text-center">
-                <img
-                  className="rounded-lg object-cover mb-3"
-                  src={category.image}
-                  alt={category.name}
-                />
                 <h2 className="text-2xl font-semibold mb-3">{category.name}</h2>
-                <p className="text-sm">Last Modified : {category.updatedAt}</p>
+                <p className="text-sm">
+                  Created : {moment(category.createdAt).fromNow()}
+                </p>
+                <p className="text-sm">
+                  Last Modified : {moment(category.updatedAt).fromNow()}
+                </p>
               </div>
             </div>
           </div>
