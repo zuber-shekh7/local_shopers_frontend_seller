9f41e228cce14c5c722518309127bda8d2d84f6a
Zuber Shekh <zuber.shekh7@gmail.com>
2022-04-13 20:43:56 +0530

    update design of breadcrumb

--- src/components/shared/Breadcrumb.js
+++ src/components/shared/Breadcrumb.js
@@ -4,18 +4,18 @@ import { HiChevronRight } from "react-icons/hi";
 
 const Breadcrumb = ({ links }) => {
   return (
-    <div className="flex mb-3">
+    <div className="flex mb-5">
       {links.map((link, index) => {
         return (
           <Link
             key={index}
-            className={`text-base sm:text-lg flex justify-center items-center space-x-1 ${
+            className={`text-xs flex justify-center items-center hover:text-indigo-700 ${
               links.length - 1 === index ? "text-indigo-600" : ""
             }`}
             to={link.to}
           >
-            <span className="font-bold capitalize">{link.name}</span>
-            <span>{links.length - 1 !== index ? <HiChevronRight /> : " "}</span>
+            <span className="font-bold uppercase">{link.name}</span>
+            <span>{links.length - 1 !== index ? <HiChevronRight /> : ""}</span>
           </Link>
         );
       })}
