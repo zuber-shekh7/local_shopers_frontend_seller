9195ed9f2187ce3eca588fd8ca676f136d7721eb
Zuber Shekh <zuber.shekh7@gmail.com>
2022-04-13 23:54:04 +0530

    update design of create category page

--- src/pages/categories/CreateCategoryPage.js
+++ src/pages/categories/CreateCategoryPage.js
@@ -5,6 +5,12 @@ import FormData from "form-data";
 import routes from "../../utils/routes";
 import { createCategory } from "../../actions/categoriesActions";
 import Breadcrumb from "../../components/shared/Breadcrumb";
+import HeaderContainer from "../../components/shared/HeaderContainer";
+import { FormGroup } from "../../components/forms/containers";
+import { Input, Label } from "../../components/forms/inputs";
+import { Button } from "../../components/buttons";
+import { Loader } from "../../components/loaders";
+import { Error } from "../../components/messages";
 
 const CreateCategoryPage = () => {
   const [name, setName] = useState("");
@@ -36,70 +42,66 @@ const CreateCategoryPage = () => {
   }
 
   return (
-    <main className="container max-w-xl">
-      <section>
+    <main>
+      <HeaderContainer>
+        <h1>Add category</h1>
+      </HeaderContainer>
+      <section className="container max-w-xl">
         {/* breadcrumb */}
         <Breadcrumb
           links={[
             {
-              name: "your account",
+              name: "home",
               to: routes.dashboard,
             },
             {
               name: "categories",
               to: routes.getCategories,
             },
-
             {
               name: "new",
               to: "",
             },
           ]}
         />
-        <div className="flex justify-center form border-0 shadow-lg">
+        <div className="flex justify-center form border rounded-lg shadow-lg">
           <div className="flex-1">
-            <div className="bg-indigo-600 py-2">
-              <h1 className="text-center text-white">Add category</h1>
-            </div>
             <form className="p-5" onSubmit={handleSubmit}>
-              <div className="mb-3">
-                <label className="block" htmlFor="name">
+              <FormGroup className="mb-3">
+                <Label className="block" htmlFor="name">
                   Name
-                </label>
-                <input
+                </Label>
+                <Input
                   id="name"
-                  className="text-lg w-full py-2 px-2 border-2 rounded-lg border-gray"
+                  className="w-full"
                   type="text"
                   value={name}
                   onChange={(e) => setName(e.target.value)}
                   placeholder="iPhones"
                   required
                 />
-              </div>
-              <div className="mb-3">
-                <label htmlFor="image">Image</label>
-                <input
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor="image">Image</Label>
+                <Input
                   id="image"
-                  className="w-full file:text-sm file:border-0 file:px-4 file:py-2 file:mt-3 file:rounded-full file:text-indigo-500 file:bg-violet-50 file:font-semibold hover:file:bg-violet-100"
+                  className="w-full"
                   type="file"
                   onChange={(e) => setImage(e.target.files[0])}
                   placeholder="Upload image"
                   accept="image/jpeg"
                   required
                 />
-              </div>
-              <div className="mb-3">
-                <button
-                  className="w-full bg-indigo-600 text-white rounded-lg px-3 py-2 mb-3 hover:bg-indigo-700"
-                  type="submit"
-                >
+              </FormGroup>
+              <FormGroup className="mb-3">
+                <Button className="w-full " type="submit">
                   Create
-                </button>
-              </div>
-              <div className="mb-3">
-                {error && <p className="text-center text-red-500">{error}</p>}
-                {loading && <p className="text-center">Loading...</p>}
-              </div>
+                </Button>
+              </FormGroup>
+              <FormGroup className="flex justify-center mb-0">
+                {error && <Error>{error}</Error>}
+                {loading && <Loader />}
+              </FormGroup>
             </form>
           </div>
         </div>
