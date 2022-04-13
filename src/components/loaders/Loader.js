d108fb9a3caf52496151c91b812ae199bdf88f55
Zuber Shekh <zuber.shekh7@gmail.com>
2022-04-13 19:48:40 +0530

    add loader component

---
+++ src/components/loaders/Loader.js
@@ -0,0 +1,11 @@
+import { RiLoader4Line } from "react-icons/ri";
+
+const Loader = () => {
+  return (
+    <span>
+      <RiLoader4Line className="animate-spin h-8 w-8 text-indigo-600" />
+    </span>
+  );
+};
+
+export default Loader;
