1aff1107eec46560cf4249f1378e11242d51f18d
Zuber Shekh <zuber.shekh7@gmail.com>
2022-04-15 08:17:46 +0530

    add helper function to generate routes

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -3,12 +3,31 @@ const routes = {
   about: "/about",
   signup: "/signup",
   login: "/login",
-  dashboard: "/seller/account",
-  getBusiness: "/seller/business",
+  dashboard: "/seller/dashboard",
+  account: "/seller/account",
+  business: "/business",
+  editBusiness: "/business/:businessId/edit",
+  getBusiness: "/business/:businessId",
   createBusiness: "/sellers/business/new",
-  createCategory: "/sellers/manage/categories/new",
-  getCategories: "/sellers/manage/categories",
-  getOrders: "/sellers/orders",
+  createCategory: "/business/:businessId/categories/new",
+  getCategories: "/business/:businessId/categories",
+  getCategory: "/business/:businessId/categories/:categoryId",
+  editCategory: "/business/:businessId/categories/:categoryId/edit",
+  getProducts: "/business/:businessId/categories/:categoryId/products",
+  createProduct: "/business/:businessId/categories/:categoryId/products/new",
+  getProduct:
+    "/business/:businessId/categories/:categoryId/products/:productId",
+  editProduct:
+    "/business/:businessId/categories/:categoryId/products/:productId/edit",
+  getOrders: "/business/:businessId/orders",
+};
+
+export const generateRoute = (str, values) => {
+  let route = str;
+  for (let key in values) {
+    route = route.replace(key, values[key]);
+  }
+  return route;
 };
 
 export default routes;
